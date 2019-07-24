64b3f4f063d868e187be893d28e0f6bea1595e5c
SingarajRai <53279295+SingarajRai@users.noreply.github.com>
2019-07-24 18:13:15 -0400

    Create ModifierJune26.java

---
+++ ModifierJune26.java
@@ -0,0 +1,21 @@
+package ifcondition;
+
+public class ModifierJune26 {
+
+	// local variable
+
+	// fields : inside tha calss but outside the main method. ( static and instance
+	// ) ( non-static)
+	static int y = 10;
+	int z = 12;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ModifierJune26 obj = new ModifierJune26(); // Creating object for modifierJune26 class
+		int x = 7;
+		System.out.println(x);
+		System.out.println(obj.z);
+
+	}
+
+}
