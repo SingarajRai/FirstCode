924de631089519d2eef2acfb80ebe785f7c76779
SingarajRai <53279295+SingarajRai@users.noreply.github.com>
2019-07-24 18:05:23 -0400

    Create DoWhileLoop.java

---
+++ DoWhileLoop.java
@@ -0,0 +1,20 @@
+package ifcondition;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Note : do { syso (i);
+		// i++;
+
+		int i = 100;
+		do {
+			System.out.println(i);
+			i = i + 1; // or i++;
+
+		} while (i <= 1000);
+
+	}
+
+}
