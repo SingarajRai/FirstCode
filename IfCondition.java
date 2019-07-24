a27b7b496282833d3e01479c12207040d77463b9
SingarajRai <53279295+SingarajRai@users.noreply.github.com>
2019-07-24 18:07:22 -0400

    Create IfCondition.java

---
+++ IfCondition.java
@@ -0,0 +1,17 @@
+package ifcondition;
+
+public class IfCondition {
+
+	public static void main(String[] args) {
+
+		String city = "Man in Work";
+		if (city.contains("Women")) {
+			System.out.println("city has the word Man");
+		} else {
+			System.out.println("city dose not have the word Women");
+
+		}
+
+	}
+
+}
