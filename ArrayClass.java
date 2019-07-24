745befe05ef66d04387894ccda96a964c000d008
SingarajRai <53279295+SingarajRai@users.noreply.github.com>
2019-07-24 18:04:15 -0400

    Create ArrayClass.java

---
+++ ArrayClass.java
@@ -0,0 +1,33 @@
+package ifcondition;
+
+public class ArrayClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] numbers = new int[5];
+		numbers[0] = 110;
+		numbers[1] = 22;
+		numbers[2] = 3;
+		numbers[3] = 77;
+		numbers[4] = 9;
+
+// find out the element of 4th index position
+		System.out.println(numbers[4]);
+
+		int[] x = new int[5];
+		x[0] = 110;
+		x[1] = 22;
+		x[2] = 3;
+		x[3] = 77;
+		x[4] = 9;
+		for (int a : x) {
+			System.out.println(a);
+
+			// Another way to careate Array
+
+		}
+
+	}
+
+}
