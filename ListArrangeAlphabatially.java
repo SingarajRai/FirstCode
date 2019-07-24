a5dcf90d075c2fc831d325f949edde5a0539d388
SingarajRai <53279295+SingarajRai@users.noreply.github.com>
2019-07-24 18:08:54 -0400

    Create ListArrangeAlphabatially.java

---
+++ ListArrangeAlphabatially.java
@@ -0,0 +1,45 @@
+package ifcondition;
+
+import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.Iterator;
+import java.util.List;
+
+public class ListArrangeAlphabatially {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<String> students = new ArrayList<String>();
+
+		students.add("Kumar");
+		students.add("Samir");
+		students.add("Rajendra");
+		students.add("Bijendra");
+		students.add("Sapin");
+		students.add("Samjhana");
+		students.add("Sajana");
+		students.add("Anjila");
+		students.add("Anika");
+		students.add("Anjana");
+		students.add("Sapin");
+		students.add("Sapin");
+		students.add("Sapin");
+
+		// Collections.sort(students);
+		// System.out.println(students);
+
+		// System.out.println(students.lastIndexOf("Sapin")); // index of sapin is 12
+
+		// for each loop :
+		// for (String x : students) { // makign a group for all strings values,
+		// combinging all names as x and making print.
+		// System.out.println(x);
+
+		// Iterator<String> myIt = students.iterator();
+
+		System.out.println(students);
+
+	}
+
+}
