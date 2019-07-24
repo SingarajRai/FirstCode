5b825b63c59131f81158f6cdac0b8ebbb413b81c
SingarajRai <53279295+SingarajRai@users.noreply.github.com>
2019-07-24 17:56:19 -0400

    Create IntArray.java

---
+++ IntArray.java
@@ -0,0 +1,24 @@
+package nine;
+
+public class IntArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] raj = { 2, 3, 444444 };
+		for (int i = 0; i < raj.length; i++) {
+			System.out.println(raj[i]);
+		}
+
+		for (int output : raj) {
+
+			System.out.println(output);
+		}
+
+		for (int output : raj) {
+			System.out.println(output);
+		}
+
+	}
+
+}
